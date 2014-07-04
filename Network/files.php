76898556e59be5893a03a504501202a0e5c485f6
Joe DF <joedf@live.ca>
2014-07-03 23:10:14 -0400

    removed non-sense comments

--- Network/files.php
+++ Network/files.php
@@ -10,8 +10,6 @@ exit();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<!-- Forked Web Design from here: http://win32.libav.org/win64/ -->
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<title>SimpleBox</title>
--- Network/login.php
+++ Network/login.php
@@ -11,8 +11,6 @@
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<!-- Forked Web Design from here: http://win32.libav.org/win64/ -->
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
 		<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
